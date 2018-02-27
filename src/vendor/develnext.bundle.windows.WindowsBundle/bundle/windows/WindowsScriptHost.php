7804cd0d4f11f8f4b520fd122fb7ee5b9786a332
Alexey <tssaltan@gmail.com>
2018-02-27 18:55:27 +0300

    PowerShell unicode fix (#18)

--- src/vendor/develnext.bundle.windows.WindowsBundle/bundle/windows/WindowsScriptHost.php
+++ src/vendor/develnext.bundle.windows.WindowsBundle/bundle/windows/WindowsScriptHost.php
@@ -89,6 +89,7 @@ class WindowsScriptHost
      * @throws WindowsException
      */
     public static function PowerShell($query, $params = [], $wait = true){
+        $query = "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8\n" . $query;
         $source = Prepare::Query($query, $params);
         $command = 'Invoke-Expression ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(\'' . base64_encode($source) . '\')))'; 
         return self::Exec(['powershell.exe', '-inputformat', 'none', '-command', $command], $wait, 'utf-8');  
