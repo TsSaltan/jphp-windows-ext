caa13265065c8d9c9607a51ed9e674581e9c98be
Alexey <tssaltan@gmail.com>
2018-04-15 19:48:52 +0300

    Update test project

--- test-project/src/app/forms/MainForm.php
+++ test-project/src/app/forms/MainForm.php
@@ -1,11 +1,14 @@
 <?php
 namespace app\forms;
 
+use Exception;
 use std, gui, framework, app;
 
 
 use bundle\windows\Windows;
 use php\gui\event\UXEvent; 
+use php\gui\event\UXWindowEvent; 
+use php\gui\event\UXMouseEvent; 
 
 class MainForm extends AbstractForm
 {
@@ -17,9 +20,30 @@ class MainForm extends AbstractForm
      * @event button4.action 
      * @event button5.action 
      */
-    function doButtonAction(UXEvent $e = null)
-    {    
+    function doButtonAction($e){    
         $this->form($e->target->text)->showAndWait();
     }
+
+    /**
+     * @event show 
+     */
+    function construct(){    
+        global $argv;
+        $this->checkbox->mouseTransparent = true;
+        $this->checkbox->selected = Windows::isAdmin();
+        $this->textArea->text = var_export($argv, true);
+    }
+
+    /**
+     * @event link.action 
+     */
+    function doLinkAction(){    
+        try{
+            Windows::requireAdmin();
+        } catch(Exception $e){
+            pre($e->getMessage());
+        }
+    }
+
 
 }
